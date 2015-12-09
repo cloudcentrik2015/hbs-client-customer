b5024d39a21fd0210898a81f9b8102d2bebdf375
java0-cloudcentrik <java0.cloudcentrik@gmail.com>
2015-12-09 19:33:37 +0100

    empty maven repository

---
+++ src/main/java/net/cloudcentrik/hbs/client/customer/Main.java
@@ -0,0 +1,13 @@
+package net.cloudcentrik.hbs.client.customer;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+   
+    public static void main(String[] args){
+        System.out.println("hbs client customer module");
+    }
+}
+
