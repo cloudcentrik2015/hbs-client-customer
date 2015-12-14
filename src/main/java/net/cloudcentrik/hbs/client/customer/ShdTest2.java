d33ce8b0f420c3cc7c0db658bda381c6d95f42e1
java1-cloudcentrik <java1.cloudcentrik@gmail.com>
2015-12-14 19:01:08 +0100

    Adding ShdTest 2 again

---
+++ src/main/java/net/cloudcentrik/hbs/client/customer/ShdTest2.java
@@ -0,0 +1,12 @@
+package net.cloudcentrik.hbs.client.customer;
+
+public class ShdTest2 {
+
+	public void test(  )
+    {
+        System.out.println( "Hello World! I am from ShdTest2" );
+		
+    }
+	
+	
+}
